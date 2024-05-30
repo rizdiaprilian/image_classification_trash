7a538bcf6b4ac7016308b7452104ada07303dbcd
rizdiaprilian <rizdi.shusuke@gmail.com>
2024-05-30 17:36:43 +0700

    Adding python scripts

---
+++ image_classification_trash/model_training.py
@@ -0,0 +1,51 @@
+import torch
+from tqdm.auto import tqdm
+from typing import Dict, List, Tuple
+from training_validation_step import model_training, model_validation
+
+
+
+def train(model: torch.nn.Module, 
+          train_dataloader: torch.utils.data.DataLoader, 
+          val_dataloader: torch.utils.data.DataLoader, 
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module,
+          epochs: int,
+          device: torch.device) -> Dict[str, List]:
+
+    # Create empty results dictionary
+    results = {"train_loss": [],
+        "train_acc": [],
+        "val_loss": [],
+        "val_acc": []
+    }
+    
+    model.to(device)
+    # Loop through training and testing steps for a number of epochs
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = model_training(model=model,
+                                          train_dataloader=train_dataloader,
+                                          loss_fn=loss_fn,
+                                          optimizer=optimizer,
+                                          device=device)
+        val_loss, val_acc = model_validation(model=model,
+                                          val_dataloader=val_dataloader,
+                                          loss_fn=loss_fn,
+                                          device=device)
+        # Print out what's happening
+        print(
+          f"Epoch: {epoch+1} | "
+          f"train_loss: {train_loss:.4f} | "
+          f"train_acc: {train_acc:.4f} | "
+          f"val_loss: {val_loss:.4f} | "
+          f"val_acc: {val_acc:.4f}"
+      )
+
+        # Update results dictionary
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["val_loss"].append(val_loss)
+        results["val_acc"].append(val_acc)
+
+    # Return the filled results at the end of the epochs
+    return results
