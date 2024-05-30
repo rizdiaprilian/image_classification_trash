e12d746792ee2fedace7b707bcfd91f001ceac99
rizdiaprilian <rizdi.shusuke@gmail.com>
2024-05-30 19:12:32 +0700

    updating 3

--- data/resize.py
+++ data/resize.py
@@ -15,7 +15,7 @@ prepath = os.path.join(os.getcwd(), 'raw', 'trash_images_train')
 class_dir = os.path.join(prepath, str(args.int_class))
 destPath = os.path.join(os.getcwd(), 'processed', 'trash_images_train_resized', args.class_name)
 
-
+print(os.getcwd())
 
 def resize(image, dim1, dim2):
 	return misc.imresize(image, (dim1, dim2))
@@ -37,4 +37,6 @@ for subdir, dirs, files in os.walk(class_dir):
         new_width, new_height = 384, 512
         resized_img = img.resize((new_width, new_height))
         resized_img.save(os.path.join(destPath, file))
+		
+print(destPath)
     
